f8b952780ea5d860d8b4e2df93f83abc6111580c
Nasya Kabakova <kabakova.nas@gmail.com>
2023-03-11 22:20:21 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String phone;
+    public String patronymic;
+    public String  surname;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
